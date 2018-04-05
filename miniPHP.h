f9114a1f7831acac8fbe233acecb3c6d860d1d26
Luis Ovalle <luispalp@hotmail.com>
2018-04-05 16:38:25 -0600

    Modificacion de expresiones regulares

---
+++ miniPHP.h
@@ -0,0 +1,31 @@
+#define p_reservada 1
+#define o_aritmetico 2
+#define o_logico 3
+#define t_logico 4
+#define t_entero 5
+#define t_reales 6
+#define t_cadena 7
+#define id 8
+#define variable 9
+#define const 10
+#define e_if 11
+#define e_then 12
+#define e_else 13
+#define e_while 14
+#define e_dowhile 15
+#define e_for 16
+#define e_foreach 17
+#define e_break 18
+#define e_swtich 19
+#define e_include 20
+#define e_continue 21
+#define e_return 22
+#define v_definidas 23
+#define funcion 24
+#define comentario 25
+#define igual 26
+#define llave 27
+#define parentesis 28
+#define corchete 29
+
+
